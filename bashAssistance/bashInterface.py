89ba99a1996982bc61ef0c9203e118eafd18771f
Matthew Biesen <upandcomming88@gmail.com>
2017-11-13 16:30:24 -0600

    Create bashInterface.py

---
+++ bashAssistance/bashInterface.py
@@ -0,0 +1,34 @@
+## relevant websites
+## https://medium.com/capital-one-developers/bashing-the-bash-replacing-shell-scripts-with-python-d8d201bc0989
+
+import subprocess
+import psutil
+
+class BashHelper:
+
+    def __init__():
+        print("bash helper initialized")
+
+    def runCommandsFromFileReturnOutput(fileWithCommands):
+        ## open file, read and execute line by line, return output
+        
+    def runBashCommand(bashCommand):
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+    def killProcess(pidfile_path):
+        try:
+            pid = int(pidfile_path.read_text())
+        except FileNotFoundError:
+            print("Process file does not exist")
+            return
+        except ValueError:
+            print("Invalid input")
+
+        try:
+            proc=psutil.Process(pid)
+            print("Killing", proc.name())
+            proc.kill()
+        except psutil.NoSuchProcess as ex:
+            print("No such process")
+
