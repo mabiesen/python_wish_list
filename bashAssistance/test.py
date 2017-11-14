90cd23fada3598a44fe0d03366f4aafe989ac4e2
Matthew Biesen <upandcomming88@gmail.com>
2017-11-14 15:52:39 -0600

    Create test.py

---
+++ bashAssistance/test.py
@@ -0,0 +1,29 @@
+import bashHelper
+
+bh = bashHelper.BashHelper()
+
+## Success
+print("test run single command")
+print(bh.run_bash_command('ls'))
+
+print(" ")
+
+## Success
+print("test run multiple commands")
+print(bh.run_commands_from_list(['ls','man scp']))
+
+
+print(" ")
+
+## Success
+print("test script directory path")
+print(bh.script_directory_path())
+
+print(" ")
+
+## Success
+print("test script directory files")
+print(bh.all_directory_files(bh.script_directory_path()))
+
+print(" ")
+
