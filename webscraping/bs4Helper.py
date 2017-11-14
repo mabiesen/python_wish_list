a56ef7fc0aa57245aa7c1fa4b52595dcde3fa2e4
Matthew Biesen <upandcomming88@gmail.com>
2017-11-13 23:52:13 -0600

    Update bs4Helper.py

--- webscraping/bs4Helper.py
+++ webscraping/bs4Helper.py
@@ -1,42 +1,50 @@
 ## idea: this file should include very basic functions which are called by reference from a list
-## the user supplies levels of depth and command as two lists.  Lists are compared by length
+## the user supplies levels of depth(int) and command list.  Listand integer compared by length
 ## used to call each function in order.  Level of depth executed top down, so user supplies
 ## first action first, etc.
 
 
 import bs4
 
+class HtmlHelper:
+    def get_links_and_linktext_from_html(stringhtml):
+        thisdict = {}
+        for link in bs4.BeautifulSoup(stringhtml, parseOnlyThese=bs4.SoupStrainer('a')):
+            if link.has_attr('href'):
+                thisdict[link.text] = link['href']
+        return thisdict
 
-def get_links_and_linktext_from_html(stringhtml):
-    thisdict = {}
-    for link in bs4.BeautifulSoup(stringhtml, parseOnlyThese=bs4.SoupStrainer('a')):
-        if link.has_attr('href'):
-            thisdict[link.text] = link['href']
-    return thisdict
+    def get_links_from_html(stringhtml):
+        thislist = []
+        for link in bs4.BeautifulSoup(stringhtml, parseOnlyThese=bs4.SoupStrainer('a')):
+            if link.has_attr('href'):
+                thislist.append(link['href'])
+        return thislist
 
-def get_links_from_html(stringhtml):
-    thislist = []
-    for link in bs4.BeautifulSoup(stringhtml, parseOnlyThese=bs4.SoupStrainer('a')):
-        if link.has_attr('href'):
-            thislist.append(link['href'])
-    return thislist
-    
- ## Get class items from html
- 
- 
- ## Get ID from html
- 
- 
- ## Get elements from html
- 
- 
- 
- ## Function switch
- 
-def functionSwitch(listFind, listLevel):
+     ## Get class items from html
 
-  for i in listlevel:
-    case(listFindt[i]):
+
+     ## Get ID from html
+
+
+     ## Get elements from html
+
+
+
+     ## Function switch
+
+    def functionSwitch(listFind, listLevel):
+      i=0
+      for i < listlevel:
+        if listFind[i] == "class":
+
+        if listFind[i] == "id":
+
+        if listFind[i] == "elem":
+
+        if listFind[i] == "link":  ## defined as 'a' and 'href'.  Commonly used so allowing this high level method
+
+        i += 1
       
  
  
