e78a9396a847ebbdf77d508506ff81a696a654e6
Matthew Biesen <upandcomming88@gmail.com>
2017-11-13 20:08:24 -0600

    Create handleConnectionDetail.py

---
+++ checkConnection/handleConnectionDetail.py
@@ -0,0 +1,8 @@
+
+
+## I want this class to offer the following:
+## 1. Check connection status (foreign, domestic)
+## 2. Obtain operating IP address
+## 3. Obtain relevant files for localized grepping:  etc/hosts, 70-persistentblahblah, firewalliptables, logs(?) ....
+## 4. Check connection strength??
+## 5. 
