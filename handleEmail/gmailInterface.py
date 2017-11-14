ba6246b8ef8616622412e5d7a2342af5543cfc5d
Matthew Biesen <upandcomming88@gmail.com>
2017-11-13 23:09:35 -0600

    Create gmailInterface.py

---
+++ handleEmail/gmailInterface.py
@@ -0,0 +1,35 @@
+import smtplib
+
+
+class GmailHelper:
+
+  gmailuser = ""
+  gmailpassword = ""
+
+  def __init__(self, user, password):
+    self.gmailuser = user
+    self.gmailpassword = password
+
+  def send_email(user, pwd, recipient, subject, body):
+      import smtplib
+
+      gmail_user = user
+      gmail_pwd = pwd
+      FROM = user
+      TO = recipient if type(recipient) is list else [recipient]
+      SUBJECT = subject
+      TEXT = body
+
+      # Prepare actual message
+      message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+      """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+      try:
+          server = smtplib.SMTP("smtp.gmail.com", 587)
+          server.ehlo()
+          server.starttls()
+          server.login(gmail_user, gmail_pwd)
+          server.sendmail(FROM, TO, message)
+          server.close()
+          print 'successfully sent the mail'
+      except:
+          print "failed to send mail"
