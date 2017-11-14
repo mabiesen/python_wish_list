ab24d11974d10e6b645849550b8b106a27f85450
Matthew Biesen <upandcomming88@gmail.com>
2017-11-13 19:33:29 -0600

    Create txtInterface.py

---
+++ handleTextFile/txtInterface.py
@@ -0,0 +1,18 @@
+
+
+## read file in whole
+
+## read file return as array of lines
+
+## write file from array
+
+## write file in whole
+
+## grep file for words
+
+## append to file
+
+## delete all from file
+
+
+
