89a5b7e045ca46ebb11800ade22a7b914d05f825
matthew <mabiesen@outlook.com>
2017-11-18 00:52:07 -0600

    more more more

---
+++ disectscript/disectScriptClass.py
@@ -0,0 +1,9 @@
+## should allow for pulling all classnames from scripts
+## option to pull all methods
+
+## should allow for pulling all function initiations
+
+## should allow for pulling all global variables
+## option to count number of global variables
+
+## SHOULD BE A CLI BASED TOOL WITH PRINT OUTPUT EXCLUSIVELY
