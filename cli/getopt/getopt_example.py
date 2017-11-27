128d7c08ede1eedf32b00990270e82bc37c65eef
matthew <mabiesen@outlook.com>
2017-11-26 19:39:19 -0600

    adjusted getopt

--- cli/getopt/getopt_example.py
+++ cli/getopt/getopt_example.py
@@ -2,27 +2,26 @@ import getopt
 import sys
 
 def command_line_arguments():
-	print 'ARGV      :', sys.argv[1:]
+    print 'ARGV      :', sys.argv[1:]
 
-	options, remainder = getopt.getopt(sys.argv[1:], 'r:s:d', ['requestlist',
-	                                                         'send',
-	                                                         'delete',
-	                                                         ])
-	print('OPTIONS   :' + options)
-	for opt, arg in options:
-		if opt in ('-r', '--requestlist'):
-			#mylist = util_message.get_user_input("Please provide the name of the list you would like to see: ")
-			option = "request"
-			print(option + ":" + arg)
+    options, remainder = getopt.getopt(sys.argv[1:], 'r:s:d', ['requestlist','send','delete'])
+    for opt, arg in options:
+        if opt in ('-r', '--requestlist'):
+            #mylist = util_message.get_user_input("Please provide the name of the list you would like to see: ")
+            option = "request"
+            print(option)
+            print(arg)
 
-		elif opt in ('-s', '--send'):
-			#mylist = util_message.get_user_input("Please provide the name of the list you would like to add to: ")
-			option = "send"
-			print(option + ":" + arg)
+        elif opt in ('-s', '--send'):
+            #mylist = util_message.get_user_input("Please provide the name of the list you would like to add to: ")
+            option = "send"
+            print(option)
+            print(arg)
 
-		elif opt in ('-d', '--delete'):
-			#mylist = util_message.get_user_input("Please provide the name of the list you would like to add to: ")
-			option = "delete"
-			print(option + ":" + arg)
+        elif opt in ('-d', '--delete'):
+            #mylist = util_message.get_user_input("Please provide the name of the list you would like to add to: ")
+            option = "delete"
+            print(option)
+            print(arg)
 
 command_line_arguments()
